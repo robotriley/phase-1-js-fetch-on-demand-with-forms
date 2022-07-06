1d931e4fb2940c64596863cfc23f7b5be2447e48
riley soloner <rileysoloner@gmail.com>
2022-07-06 09:27:52 -0400

    commit tree b782954251bc6fdbb8cb10e511e6a204f466a8b4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        
+    });
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
